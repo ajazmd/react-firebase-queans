e6e199e9b6a6095ad453753a9fe6eb6cda81404f
mdajaz <ajazansari11102@gmail.com>
2025-02-16 01:50:20 +0530

    new firebase initialization setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAY9gazk0zPSzIPyCX-XBMkHwdYpY2ONnY",
-  authDomain: "questionanswerapp-6aaf7.firebaseapp.com",
-  projectId: "questionanswerapp-6aaf7",
-  storageBucket: "questionanswerapp-6aaf7.firebasestorage.app",
-  messagingSenderId: "457536283403",
-  appId: "1:457536283403:web:550b251d95b4ae9ab7b80e",
-  measurementId: "G-WTWZK30VLQ"
+  apiKey: "AIzaSyDeHi5xD7hddorLzn_8C_maeM6AasmF70o",
+  authDomain: "quickask-cad0c.firebaseapp.com",
+  projectId: "quickask-cad0c",
+  storageBucket: "quickask-cad0c.firebasestorage.app",
+  messagingSenderId: "688012718721",
+  appId: "1:688012718721:web:c07b746b8cecfb9d0b1380"
 };
 
 
